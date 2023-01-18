dadabdec33648f1588596a69fc9eaf5d35d43d5e
Ani Gamtsemlidze <ani.gamcemlidze20@gmail.com>
2023-01-18 15:48:18 +0400

    responsive

---
+++ main.js
@@ -0,0 +1,28 @@
+var name = "anii";
+
+var age = 10
+// console.log(age)
+
+if(age > 18){
+    
+    console.log("me var sruwlovani")
+}
+else if (name == "ani") {
+
+    console.log("ar var sruwlovani mara mqvia ani")
+}
+else {
+    console.log("me ar var sruwlovani")
+
+}
+
+var a = 4;
+var b = 8;
+var c  = 0;
+console.log(c)
+
+c=a+b+b
+
+console.log(c)
+
+
